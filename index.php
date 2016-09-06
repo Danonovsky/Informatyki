9d861649b7af265f9bd6735e883f45bc57a7cbef
Danonovsky <danonovsky@gmail.com>
2016-09-06 19:46:48 +0200

    Titel dodany

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
+<title>Kebab</title>
 <?php
 
 echo "Emil robi dobrze za browarka<br>";
-$dupa="Emila";
+$dupa="Emaila";
 if($dupa=="Emila")
 {
     echo "Jebać unię i resztę pedalstwa, Korwin krul";
+}
+else
+{
+    echo "Marksiści to kurwy i złodzieje";
 }
 ?>
