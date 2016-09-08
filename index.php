4745979661af0b2fd3694cd4580cf83cebb67389
HiImMishu <michal.misiak@op.pl>
2016-09-08 19:19:35 +0200

    Merge branch 'master' of https://github.com/Danonovsky/Informatyki into test
    # Conflicts:
    #	index.php
    # zaloguj.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <form action="zaloguj.php" method="POST">
       Login: <br/> <input type="text" name="login"/><br/>
       Hasło <br/>  <input type="password" name="haslo"/><br/><br/>
-      <input type="submit" value="Zaloguj się"/>
+      <input type="submit" value="Zaloguj się" name="submit"/>
     </form>
 
 
---
+++ zaloguj.php
@@ -0,0 +1,8 @@
+<?php
+
+$login = $_POST['login'];
+$passwd = $_POST["haslo"];
+
+echo $login."<br/>";
+echo $passwd;
+?>
