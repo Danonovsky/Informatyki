9e76cdcac3247c81ad6520497b70d55dd4cddedd
HiImMishu <michal.misiak@op.pl>
2016-09-08 18:47:56 +0200

    Merge branch 'master' of https://github.com/Danonovsky/Informatyki into test
    # Conflicts:
    #	index.php

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
-<title>Kebab</title>
-<?php
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="x-ua-compatible" content="IE=edge,chrome=1"/>
+    <title>Osadnicy Bitch</title>
+</head>
 
-echo "Emil robi dobrze za browarka<br>";
-$dupa="Emaila";
-if($dupa=="Emila")
-{
-    echo "Jebać unię i resztę pedalstwa, Korwin krul";
-}
-else
-{
-    echo "Marksiści to kurwy i złodzieje";
-}
-?>
+<body>
+Tylko martwi ujrzeli koniec wojny - Platon<br/><br/>
+  <form action="zaloguj.php" method="post">
+    Login: <br/> <input type="text" name="login"/><br/>
+    Hasło <br/>  <input type="password" name="haslo"/><br/><br/>
+    <input type="submit" value="Zaloguj się"/>
+  </form>
+
+
+</body>
+</html>
