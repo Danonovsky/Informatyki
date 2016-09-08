4a7bad1addb49f42e5c8d3831b2c832de638f31c
HiImMishu <michal.misiak@op.pl>
2016-09-08 19:00:56 +0200

    Merge branch 'master' of https://github.com/Danonovsky/Informatyki into test
    # Conflicts:
    #	index.php
    # zaloguj.php

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <!DOCTYPE HTML>
 <html lang="pl">
-<head>
+  <head>
     <meta charset="utf-8"/>
     <meta http-equiv="x-ua-compatible" content="IE=edge,chrome=1"/>
     <title>Osadnicy Bitch</title>
-</head>
+  </head>
 
-<body>
-Tylko martwi ujrzeli koniec wojny - Platon<br/><br/>
-  <form action="zaloguj.php" method="post">
-    Login: <br/> <input type="text" name="login"/><br/>
-    Hasło <br/>  <input type="password" name="haslo"/><br/><br/>
-    <input type="submit" value="Zaloguj się"/>
-  </form>
+  <body>
+  Tylko martwi ujrzeli koniec wojny - Platon<br/><br/>
+    <form action="zaloguj.php" method="POST">
+      Login: <br/> <input type="text" name="login"/><br/>
+      Hasło <br/>  <input type="password" name="haslo"/><br/><br/>
+      <input type="submit" value="Zaloguj się"/>
+    </form>
 
 
-</body>
+  </body>
 </html>
