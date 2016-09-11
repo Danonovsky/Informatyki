81bfc436a0cc00cd8cab4dc733a259063d4e2dde
Danonovsky <danonovsky@gmail.com>
2016-09-11 15:20:57 +0200

    Szkielet jako taki

--- index.php
+++ index.php
@@ -1,20 +1,21 @@
+<?php
+session_start()
+?>
 <!DOCTYPE HTML>
 <html lang="pl">
   <head>
     <meta charset="utf-8"/>
     <meta http-equiv="x-ua-compatible" content="IE=edge,chrome=1"/>
-    <title>Osadnicy Bitch</title>
+    <title>Portal taki do zdjęć</title>
   </head>
-
   <body>
-  Tylko martwi ujrzeli koniec wojny - Platon<br/><br/>
-    <form action="zaloguj.php" method="POST">
-      Login: <br/> <input type="text" name="login"/><br/>
-      Hasło <br/>  <input type="password" name="haslo"/><br/><br/>
+    <form action="login.php" method="POST">
+      Login: <input type="text" name="login"/>
+      Hasło: <input type="password" name="password"/>
       <input type="submit" value="Zaloguj się"/>
     </form>
-  <?php
-  phpinfo();
-  ?>
+    <br/><a href="register.php">Nie posiadasz konta? Zarejestruj się!</a>
+  <hr/><br/><br/>
+  <center><h1>Strona do zdjęć</h1></center>
   </body>
 </html>
---
+++ register.php
@@ -0,0 +1,27 @@
+<?php
+session_start()
+?>
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="x-ua-compatible" content="IE=edge,chrome=1"/>
+    <title>Portal taki do zdjęć - rejestracja</title>
+</head>
+<body>
+<center><h1>Strona do zdjęć - rejestracja</h1></center><br/><br/>
+<form method="post">
+    <center>
+        Login: <input type="text" name="login"/><br/><br/>
+        Hasło: <input type="text" name="login"/><br/><br/>
+        Powtórz hasło: <input type="text" name="login"/><br/><br/>
+        Nazwa wyświetlana: <input type="text" name="login"/><br/><br/>
+        E-mail: <input type="text" name="login"/><br/><br/>
+        <input type="submit" value="Zarejestruj się"/>
+
+    </center>
+</form>
+<br/>
+<center><a href="index.php"><input type="button" value="Powrót"/></a></center>
+</body>
+</html>
